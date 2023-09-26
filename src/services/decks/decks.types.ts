b94b1198c512221a0c07ade954ac2a3e92e15c76
AndreyVialichka <forruexpress12@gmail.com>
2023-09-26 19:44:18 +0300

    decks

--- src/services/decks/decks.types.ts
+++ src/services/decks/decks.types.ts
@@ -1,34 +1,37 @@
-export type Pagination = {
-    totalPages: number
-    currentPage: number
-    itemsPerPage: number
-    totalItems: number
-  }
-  
-  export type Author = {
-    id: string
-    name: string
-  }
-  
-  export type Deck = {
-    id: string
-    userId: string
-    name: string
-    isPrivate: boolean
-    shots: number
-    cover?: string | null
-    rating: number
-    isDeleted: boolean | null
-    isBlocked?: boolean | null
-    created: string
-    updated: string
-    cardsCount: number
-    author: Author
-  }
-  
-  export type DecksResponse = {
-    maxCardsCount: number
-    pagination: Pagination
-    items: Deck[]
-  }
-  
+import { PaginatedEntity, PaginatedRequest } from '../types'
+
+export type GetDecksArgs = PaginatedRequest<{
+  minCardsCount?: number
+  maxCardsCount?: number
+  name?: string
+  authorId?: Author['id']
+  orderBy?: string
+}>
+
+export type CreateDeckArgs = {
+  name: string
+}
+export interface Author {
+  id: string
+  name: string
+}
+
+export interface Deck {
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  shots: number
+  cover: string | null
+  rating: number
+  isDeleted: boolean | null
+  isBlocked: boolean | null
+  created: string
+  updated: string
+  cardsCount: number
+  author: Author
+}
+
+export type DecksResponse = PaginatedEntity<Deck> & {
+  maxCardsCount: number
+}
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
- 
-import { baseApi } from './base-api'
- 
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+
+import { baseApi } from '../services/base-api'
+import { decksSlice } from '../services/decks/decks.slice'
+
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
+    [decksSlice.name]: decksSlice.reducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
 })
- 
+
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
